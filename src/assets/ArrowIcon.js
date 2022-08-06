346625c9a6ec5fc5ec88a0b73694f7cfd2f8406c
osadezu <osadezu@gmail.com>
2022-08-06 12:52:57 -0400

    Change arrow svg to a jsx component

---
+++ src/assets/ArrowIcon.js
@@ -0,0 +1,13 @@
+export const ArrowIcon = () => (
+  <svg
+    width='18'
+    height='15'
+    viewBox='0 0 18 14'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'>
+    <path
+      d='M9.8995 14.0716L16.9706 7.00049M16.9706 7.00049L14.6135 7.00049L0 7.00049M16.9706 7.00049L10.4887 0.518674'
+      stroke='currentColor'
+    />
+  </svg>
+);
